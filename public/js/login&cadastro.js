4ef6ec6c1cca013485ff13a76f786eaefe1c0a28
Miguel-SpTech <miguel.mauer@sptech.school>
2026-05-14 10:21:58 -0300

    alpha version

---
+++ public/js/login&cadastro.js
@@ -0,0 +1,11 @@
+let name_register = document.getElementById("inp_register_name");
+let nick_register = document.getElementById("inp_register_nick");
+let email_register = document.getElementById("inp_register_email");
+let password_register = document.getElementById("inp_register_password");
+let password_confirmation_register = document.getElementById("inp_register_password_confirmation");
+let fotoperfil_register = document.getElementById("inp_register_fotoperfil");
+
+let email_login = document.getElementById("inp_login_email");
+let password_login = document.getElementById("inp_login_password");
+
+
