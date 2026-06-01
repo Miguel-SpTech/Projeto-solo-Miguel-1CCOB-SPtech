2d968a9c35ddffb62e810bbb84df00caeb38b8f6
Miguel-SpTech <miguel.mauer@sptech.school>
2026-06-01 01:09:44 -0300

    fixing home page and gym leaders

--- public/js/functions.js
+++ public/js/functions.js
@@ -11,7 +11,8 @@ let pokemon_name = document.getElementById("pokemonName");
  
 let pokemon  = 0;
 var route    = 1;
-let pokelife = 0;
+let life = 0;
+let lifeMax = 0;
  
 // ──────────────────────────────────────────────────────────────────────────────
 let player = {
@@ -103,39 +104,8 @@ function carregarTrofeusDoBanco() {
         });
 }
  
-// ──────────────────────────────────────────────────────────────────────
-function updateData() {
-    document.getElementById("msm_qtdPokeball").innerHTML = `Quantidade de Pokebolas: ${player.qtdPokeball}`;
-    document.getElementById("H3life").innerHTML = `Life: ${player.life}`;
-    document.getElementById("H3damage").innerHTML = `Damage: ${player.damage}`;
-}
- 
-setInterval(updateData, 1000);
- 
 // ─────────────────────────────────────────────────────────────────────────
-async function updatePokedex() {
-    console.log("Pokedex Update started");
- 
-    for (let p = 1; p < 152; p++) {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${p}`);
-        const data     = await response.json();
- 
-        if (pokemons[p].registered == true) {
-            if (pokemons[p].shinyregistered == true) {
-                document.getElementById(`pokedexN${p}`).src          = data.sprites.front_shiny;
-                document.getElementById(`spanPokedexN${p}`).innerHTML = pokemons[p].name;
-            } else {
-                document.getElementById(`pokedexN${p}`).src          = data.sprites.front_default;
-                document.getElementById(`spanPokedexN${p}`).innerHTML = pokemons[p].name;
-            }
-        }
-    }
- 
-    console.log("Pokedex Update finished");
-}
- 
-setInterval(updatePokedex, 10000);
- 
+
 // ──────────────────────────────────────────────────────────────────
 function rng(max, min) {
     let random = Math.random() * (max - min + 1) + min;
@@ -185,7 +155,6 @@ function catchPoke() {
  
         salvarTreiner();
     } else {
-
         salvarTreiner();
     }
  
@@ -230,15 +199,143 @@ async function findPokemon1() {
     let shinychance = rng(8192, 1).toFixed(0);
     if (pokemon > 151) { pokemon = 151; }
  
-    pokelife = pokemons[pokemon].life;
+    lifeMax = pokemons[pokemon].life;
+    life = pokemons[pokemon].life;
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
  
     if (pokemons[pokemon].canFind == true) {
         if (shinychance >= 8192) {
-            img1.src          = data.sprites.versions['generation-v']['black-white'].animated.front_shiny;
+            img1.src = data.sprites.versions['generation-v']['black-white'].animated.front_shiny;
             pokemon_name.innerHTML = `${pokemons[pokemon].name} ✨`;
         } else {
-            img1.src          = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+            img1.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
             pokemon_name.innerHTML = `${pokemons[pokemon].name}`;
         }
     }
+}
+
+function brock() {
+    img1.src = 'assets/img/brock.jpg';
+    document.getElementById("spanRoute").innerHTML = "1º Gym";
+    lifeMax = lideres[0].maxLife;
+    life = lideres[0].life;
+    document.getElementById("pokemonName").innerHTML = "Brock";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function misty() {
+    img1.src = 'assets/img/misty.jpg';
+    document.getElementById("spanRoute").innerHTML = "2º Gym"
+    lifeMax = lideres[1].maxLife;
+    life = lideres[1].life;
+    document.getElementById("pokemonName").innerHTML = "Misty";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function ltSurge() {
+    img1.src = 'assets/img/lt-surge.jpg';
+    document.getElementById("spanRoute").innerHTML = "3º Gym"
+    lifeMax = lideres[2].maxLife;
+    life = lideres[2].life;
+    document.getElementById("pokemonName").innerHTML = "lt. Surge";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function erika() {
+    img1.src = 'assets/img/erika.jpg';
+    document.getElementById("spanRoute").innerHTML = "4º Gym"
+    lifeMax = lideres[3].maxLife;
+    life = lideres[3].life;
+    document.getElementById("pokemonName").innerHTML = "Erika";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function koga() {
+    img1.src = 'assets/img/koga.jpg';
+    document.getElementById("spanRoute").innerHTML = "5º Gym"
+    lifeMax = lideres[4].maxLife;
+    life = lideres[4].life;
+    document.getElementById("pokemonName").innerHTML = "Koga";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function sabrina() {
+    img1.src = 'assets/img/sabrina.jpg';
+    document.getElementById("spanRoute").innerHTML = "6º Gym"
+    lifeMax = lideres[5].maxLife;
+    life = lideres[5].life;
+    document.getElementById("pokemonName").innerHTML = "Sabrina";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function blaine() {
+    img1.src = 'assets/img/blaine.jpg';
+    document.getElementById("spanRoute").innerHTML = "7º Gym"
+    lifeMax = lideres[6].maxLife;
+    life = lideres[6].life;
+    document.getElementById("pokemonName").innerHTML = "Blaine";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function giovanni() {
+    img1.src = 'assets/img/giovanni.jpg';
+    document.getElementById("spanRoute").innerHTML = "8º Gym"
+    lifeMax = lideres[7].maxLife;
+    life = lideres[7].life;
+    document.getElementById("pokemonName").innerHTML = "Giovanni";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function lorelei() {
+    img1.src = 'assets/img/ltSurge.jpg';
+    document.getElementById("spanRoute").innerHTML = "1º Elite"
+    lifeMax = lideres[8].maxLife;
+    life = lideres[8].life;
+    document.getElementById("pokemonName").innerHTML = "Lorelei";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function bruno() {
+    img1.src = 'assets/img/ltSurge.jpg';
+    document.getElementById("spanRoute").innerHTML = "2º Elite"
+    lifeMax = lideres[9].maxLife;
+    life = lideres[9].life;
+    document.getElementById("pokemonName").innerHTML = "Bruno";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function agatha() {
+    img1.src = 'assets/img/ltSurge.jpg';
+    document.getElementById("spanRoute").innerHTML = "3º Elite"
+    lifeMax = lideres[10].maxLife;
+    life = lideres[10].life;
+    document.getElementById("pokemonName").innerHTML = "Agatha";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function lance() {
+    img1.src = 'assets/img/ltSurge.jpg';
+    document.getElementById("spanRoute").innerHTML = "4º Elite"
+    lifeMax = lideres[11].maxLife;
+    life = lideres[11].life;
+    document.getElementById("pokemonName").innerHTML = "Lance";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function blue() {
+    img1.src = 'assets/img/ltSurge.jpg';
+    document.getElementById("spanRoute").innerHTML = "Champeon"
+    lifeMax = lideres[12].maxLife;
+    life = lideres[12].life;
+    document.getElementById("pokemonName").innerHTML = "Blue";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
+}
+
+function red() {
+    img1.src = 'assets/img/red.jpg';
+    document.getElementById("spanRoute").innerHTML = "Mt. Silver"
+    lifeMax = lideres[13].maxLife;
+    life = lideres[13].life;
+    document.getElementById("pokemonName").innerHTML = "...";
+    document.getElementById("hpText").innerHTML = `${life} / ${lifeMax}`
 }
---
+++ public/js/lideres.js
@@ -0,0 +1,17 @@
+let lideres = [
+    {'name':'brock','src':'assets/img/brock.jpg','maxLife':75,'life':75,'damage':20},
+    {'name':'misty','src':'assets/img/misty.jpg','maxLife':125,'life':125,'damage':35},
+    {'name':'lt-surge','src':'assets/img/lt-surge.jpg','maxLife':155,'life':155,'damage':45},
+    {'name':'erika','src':'assets/img/erika.jpg','maxLife':185,'life':185,'damage':55},
+    {'name':'koga','src':'assets/img/koga.jpg','maxLife':200,'life':200,'damage':65},
+    {'name':'sabrina','src':'assets/img/sabrina.jpg','maxLife':200,'life':200,'damage':65},
+    {'name':'blaine','src':'assets/img/blaine.jpg','maxLife':225,'life':225,'damage':75},
+    {'name':'giovanni','src':'assets/img/giovanni.jpg','maxLife':245,'life':245,'damage':90},
+    {'name':'lorelei','src':'assets/img/lorelei.jpg','maxLife':260,'life':260,'damage':90},
+    {'name':'bruno','src':'assets/img/bruno.jpg','maxLife':260,'life':260,'damage':90},
+    {'name':'agatha','src':'assets/img/agatha.jpg','maxLife':260,'life':260,'damage':90},
+    {'name':'lance','src':'assets/img/lance.jpg','maxLife':260,'life':260,'damage':90},
+    {'name':'blue','src':'assets/img/blue.jpg','maxLife':280,'life':280,'damage':100},
+    {'name':'red','src':'assets/img/red.jpg','maxLife':300,'life':300,'damage':110},
+]
+
